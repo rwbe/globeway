636476d36b470e8f2ff7de1a02745ba3e3d26a16
Ricardo Willian <rwbemiliano@gmail.com>
2025-11-06 06:31:50 -0300

    update ESLint configuration for TypeScript and React
    - Modernize ESLint setup with latest TypeScript and React rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,30 @@
+// eslint.config.js
 import js from '@eslint/js';
 import globals from 'globals';
+import tseslint from 'typescript-eslint';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'build'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+    ],
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
